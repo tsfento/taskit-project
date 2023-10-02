9a80c10a117df310ac14e539b54e17301a72b6e1
Tyler Fenton <tsfento@gmail.com>
2023-10-02 14:56:42 -0500

    Laying out sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TasksListComponent } from './tasks-list/tasks-list.component';
+import { UserInfoBoxComponent } from './sidebar/user-info-box/user-info-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    TasksListComponent,
+    UserInfoBoxComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar/user-info-box/user-info-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-info-box',
+  templateUrl: './user-info-box.component.html',
+  styleUrls: ['./user-info-box.component.css']
+})
+export class UserInfoBoxComponent {
+
+}
