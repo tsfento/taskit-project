29ba9a2a043f988c2d451bdc9dd8c5d12d57044c
Tyler Fenton <tsfento@gmail.com>
2023-10-04 14:34:57 -0500

    Testing empy tasks

--- src/app/tasks-list/tasks-list.component.ts
+++ src/app/tasks-list/tasks-list.component.ts
@@ -12,7 +12,8 @@ export class TasksListComponent {
   tasks: Task[] = [
     new Task('Clean Out Garage', '10/02/23', 'Low', 'To Do'),
     new Task('Do Dishes', '10/04/23', 'Medium', 'To Do'),
-    new Task('Pay Bills', '10/10/23', 'High', 'To Do')
+    new Task('Pay Bills', '10/10/23', 'High', 'To Do'),
+    new Task('', '', '', '')
   ];
 
   showModal(bool) {
