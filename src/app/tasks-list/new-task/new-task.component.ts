fcb25b05cbd9cb79da8150e00edace31d5a3e515
Tyler Fenton <tsfento@gmail.com>
2023-10-05 15:08:37 -0500

    Laid out modal for adding new task

--- src/app/tasks-list/new-task/new-task.component.ts
+++ src/app/tasks-list/new-task/new-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit, OnChanges } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/shared/task.model';
 
 declare var window;
 
@@ -8,17 +9,36 @@ declare var window;
   styleUrls: ['./new-task.component.css']
 })
 export class NewTaskComponent implements OnInit, OnChanges {
+  @Output() sendTask = new EventEmitter<Task>();
+  task: Task = new Task('','','','');
+
   @Input() toShow: boolean;
   newTaskModal;
 
   ngOnInit() {
     this.newTaskModal = new window.bootstrap.Modal(document.getElementById('newTaskModal'));
-    // this.newTaskModal.show();
+    this.newTaskModal.show();
   }
 
   ngOnChanges(toShow) {
     if (toShow) {
       this.newTaskModal.show();
     }
+  }
+
+  addTask() {
+    const form: HTMLFormElement = document.querySelector('#taskForm');
+    const taskName: HTMLInputElement = document.querySelector('#inputTaskName');
+    const taskDetails: HTMLInputElement = document.querySelector('#inputDetails');
+    const taskDueDate: HTMLInputElement = document.querySelector('#inputDueDate');
+    const taskPriority: HTMLInputElement = document.querySelector('#inputPriority');
+    const taskStatus: HTMLInputElement = document.querySelector('#inputStatus');
+
+    this.task.title = taskName.value;
+    this.task.dueDate = taskDueDate.value;
+    this.task.priority = taskPriority.value;
+    this.task.status = taskStatus.value;
+
+    form.reset();
   }
 }
