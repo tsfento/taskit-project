1721cf6878055b7a4159302f45c22f5759fb0983
Tyler Fenton <tsfento@gmail.com>
2023-10-16 11:19:23 -0500

    Refactored KanBan

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -18,66 +18,14 @@ export class KanbanComponent implements OnInit {
   ngOnInit(): void {
     this.tasks = this.tasksService.getTasks();
 
-    this.initTasks();
-
-    this.separateTasks();
-
-    this.sortTasks(this.toDoTasks);
-    this.sortTasks(this.inProgressTasks);
-    this.sortTasks(this.doneTasks);
-
     this.tasksService.tasksChanged.subscribe(
       (changedTasks: Task[]) => {
         this.tasks = changedTasks;
-
-        this.initTasks();
-
-        this.separateTasks();
-
-        this.sortTasks(this.toDoTasks);
-        this.sortTasks(this.inProgressTasks);
-        this.sortTasks(this.doneTasks);
       }
     )
   }
 
-  initTasks() {
-    this.toDoTasks = [];
-    this.inProgressTasks = [];
-    this.doneTasks = [];
-  }
-
-  sortTasks(taskArray: Task[]) {
-    taskArray.sort(
-      (a, b) => {
-        let x = a.unformattedDate;
-        let y = b.unformattedDate;
-        if (x < y) { return -1; }
-        if (x > y) { return 1; }
-      }
-    );
-  }
-
-  separateTasks() {
-    for (let i = 0; i < this.tasks.length; i++) {
-      if (this.tasks[i].statusNumber === 1) {
-        this.tasks[i].index = i;
-        this.toDoTasks.push(this.tasks[i]);
-      }
-
-      if (this.tasks[i].statusNumber === 2) {
-        this.tasks[i].index = i;
-        this.inProgressTasks.push(this.tasks[i]);
-      }
-
-      if (this.tasks[i].statusNumber === 3) {
-        this.tasks[i].index = i;
-        this.doneTasks.push(this.tasks[i]);
-      }
-    }
-  }
-
-  changeStatus(status: string, index: number, taskArray: Task[]) {
-    this.tasksService.changeStatus(status, taskArray[index].index);
+  changeStatus(status: string, index: number) {
+    this.tasksService.changeStatus(status, index);
   }
 }
