be5959ecb3752ae091dac7806fb57ee4ab2fafee
Tyler Fenton <tsfento@gmail.com>
2023-10-25 10:59:12 -0500

    Added basic functionality for dragging and dropping KanBan tasks

--- src/app/kanban/kanban.component.ts
+++ src/app/kanban/kanban.component.ts
@@ -11,6 +11,7 @@ import { Subscription } from 'rxjs';
 export class KanbanComponent implements OnInit, OnDestroy {
   tasks: Task[] = [];
   tasksSub: Subscription;
+  draggingTask: Task;
 
   constructor(private tasksService: TasksService) {}
 
@@ -30,5 +31,29 @@ export class KanbanComponent implements OnInit, OnDestroy {
 
   changeStatus(status: string, index: number) {
     this.tasksService.changeStatus(status, index);
+  }
+
+  onDragStart(index: number) {
+    this.draggingTask = this.tasks[index];
+  }
+
+  onDragOver(event: Event) {
+    event.preventDefault();
+    // console.log(event.target);
+  }
+
+  onDrop(event: Event) {
+    const element = event.target as HTMLElement;
+
+    if (element.classList.contains('todo-container')) {
+      this.draggingTask.status = 'To Do';
+      this.draggingTask.statusNumber = 1;
+    } else if (element.classList.contains('progress-container')) {
+      this.draggingTask.status = 'In Progress';
+      this.draggingTask.statusNumber = 2;
+    } else if (element.classList.contains('done-container')) {
+      this.draggingTask.status = 'Done';
+      this.draggingTask.statusNumber = 3;
+    }
   }
 }
